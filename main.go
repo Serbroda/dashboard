9c71669fc07355ee0373085eb26c5c851317cffd
Danny Rottstegge <danny@rottstegge.net>
2023-03-30 21:58:27 +0200

    init: svelte frontend

--- main.go
+++ main.go
@@ -1,41 +1,27 @@
 package main
 
 import (
-	"errors"
-	"github.com/Serbroda/dashboard/handler"
+	"embed"
 	"github.com/labstack/echo/v4"
-	"html/template"
-	"io"
 )
 
-type TemplateRegistry struct {
-	templates map[string]*template.Template
-}
+var (
+	//go:embed frontend/dist
+	FrontendDist embed.FS
+	//go:embed frontend/dist/index.html
+	IndexHTML embed.FS
+)
 
-// Implement e.Renderer interface
-func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	tmpl, ok := t.templates[name]
-	if !ok {
-		err := errors.New("Template not found -> " + name)
-		return err
-	}
-	return tmpl.ExecuteTemplate(w, "base.html", data)
-}
+var (
+	distDirFS     = echo.MustSubFS(FrontendDist, "frontend/dist")
+	distIndexHtml = echo.MustSubFS(IndexHTML, "frontend/dist")
+)
+
 func main() {
 	e := echo.New()
 
-	// Instantiate a template registry with an array of template set
-	// Ref: https://gist.github.com/rand99/808e6e9702c00ce64803d94abff65678
-	templates := make(map[string]*template.Template)
-	templates["index.html"] = template.Must(template.ParseFiles("view/index.html", "view/layout.html"))
-	//templates["about.html"] = template.Must(template.ParseFiles("view/about.html", "view/base.html"))
-	e.Renderer = &TemplateRegistry{
-		templates: templates,
-	}
-
-	// Route => handler
-	e.GET("/", handler.HomeHandler)
-	//e.GET("/about", handler.AboutHandler)
+	e.StaticFS("/", distDirFS)
+	e.FileFS("/", "index.html", distIndexHtml)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
