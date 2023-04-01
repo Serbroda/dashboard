e244102daa3fc30fe61f0413f82d8a5612fbccdc
Danny Rottstegge <danny@rottstegge.net>
2023-04-01 22:32:40 +0200

    wip: styling

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,23 +1,32 @@
 <script lang="ts">
-import {apiService} from "./services/api.service.js";
+    import {apiService} from "./services/api.service.js";
+    import Section from "./lib/Section.svelte";
 </script>
 
 <svelte:head>
-{#await apiService.get() then data}
-    <title>{data.title}</title>
-{/await}
+    {#await apiService.get() then data}
+        <title>{data.title}</title>
+    {/await}
 </svelte:head>
 
 <main>
-  <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
 
-  <div class="container">
-    {#each [...Array(10).keys()] as i}
-      <section class="section">{i+1}</section>
-    {/each}
-  </div>
+    <div class="container">
+        {#await apiService.get() then data}
+
+            {#each data.sections || [] as section}
+                <Section title={section.name} items={section.items}/>
+            {/each}
+        {/await}
+    </div>
 </main>
 
 <style>
-
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        gap: 4px;
+        padding: 0px 12px;
+    }
 </style>
---
+++ frontend/src/lib/Section.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let title: string = "";
+  export let items: any[] = [];
+</script>
+
+<div class="section--container">
+  <div class="section--header">
+    {title}
+  </div>
+  <div class="section--items-container">
+    {#each items as item}
+      <a class="section--item" href={item.url}>{item.name}</a>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .section--container {
+    display: flex;
+    flex-direction: column;
+    padding: 8px;
+    border-radius: 6px;
+    background-color: #2f2f2f;
+  }
+  .section--header {
+  }
+  .section--items-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 4px;
+    margin-top: 4px;
+  }
+  .section--item {
+    padding: 8px;
+    border-radius: 6px;
+    background-color: #1f1f1f;
+  }
+</style>
